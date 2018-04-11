f233a54ec50c678dd182b06da31a44f977994ca7
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-11 18:19:59 -0300

    Variável INT e Concatenação

--- Aula_04.java
+++ Aula_04.java
@@ -1,16 +1,19 @@
+//Criando o pacote
 package aulas;
 
+//Criando a Classe
 public class Aula_04{
     
+    //Criando o método principal
     public static void main(String[]args){
         
-        //Declaração da variável x que pode ARMAZENAR valores do TIPO INTEIRO
+        //Declaração da variável x que pode ARMAZENAR valores do TIPO INTEIRO(int)
         int x;
         //Declaração do valor inteiro para x
         x=20;
         
         //Exibe na tela do usuário o CONTEÚDO da vatiável x, que no caso é 20
-        //Simbulo + faz a concatenação 
+        //Símbulo + faz a concatenação, ou seja, pode unir o conteúdo antes ou depois da variável x. 
         System.out.println("Você tem " + x + " anos");
     }
 }
