b9960eb1cc763ec5571f41d1f4c3d0956b51b759
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-11 16:23:19 -0300

    Variáveis e Concatenação

---
+++ Aula_04.java
@@ -0,0 +1,16 @@
+package aulas;
+
+public class Aula_04{
+    
+    public static void main(String[]args){
+        
+        //Declaração da variável x que pode ARMAZENAR valores do TIPO INTEIRO
+        int x;
+        //Declaração do valor inteiro para x
+        x=20;
+        
+        //Exibe na tela do usuário o CONTEÚDO da vatiável x, que no caso é 20
+        //Simbulo + faz a concatenação 
+        System.out.println("Você tem " + x + " anos");
+    }
+}
