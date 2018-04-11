79cd8d99b73e7b2eaca08e0f4f77fffc35bc6167
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-11 19:41:58 -0300

    Operadores Aritméticos

---
+++ Aula_05.java
@@ -0,0 +1,32 @@
+package aulas;
+
+//Classe
+public class Aula_05{
+    
+    //Método principal
+    public static void main(String[]args){
+        
+        //Variáveis
+        int x;
+        x=20;
+        
+        int y=200;
+        
+        //Operadores Aritméticos
+        int soma = x+y;
+        int subtrai = x-y;
+        int multiplica = x*y;
+        int dividir = x/y;
+        
+        
+        //Exibe na tela do usuário
+        System.out.println("A soma de x com y: " + soma);
+        System.out.println("A subtração de x com y: " + subtrai);
+        System.out.println("A multiplicação de x com y: " + multiplica);
+        System.out.println("A divisão de x com y: " + dividir);
+    }
+    
+    
+    
+    
+}
