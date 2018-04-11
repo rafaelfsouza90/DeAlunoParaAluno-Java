f5bd6be94218b45c696ca2ab022361b1a429ac1b
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-11 16:10:59 -0300

    Update Aula_01.java

--- Aula_01.java
+++ Aula_01.java
@@ -1,9 +1,12 @@
 package aulas;
 
+//Criando a classe 
 public class Aula_01{
-        
+    
+    //MÉTODO PRINCIPAL informa o ponto de inicio do programa
     public static void main(String[]args){
         
+        //Introduzindo mensagem na tela do usuário    
         System.out.println("Pao com mortadela");
     }
     
