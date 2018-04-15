7162594339b3fdf0f1d578d35d2632c57cbcb056
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-15 13:18:39 -0300

    Variável double para números decimais

---
+++ Aula_06.java
@@ -0,0 +1,17 @@
+package aulas;
+public class Aula_06{
+    
+    public static void main(String[]args){
+        
+        int x;
+        x = 150;
+        
+        double y = 10.5;
+        
+        double resultado;
+        resultado = x + y;
+        
+        System.out.println(resultado);
+    }
+    
+}
