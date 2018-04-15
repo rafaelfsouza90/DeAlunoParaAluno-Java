c61dcf5d04b84b38474d6d006feec896c26072e1
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-15 14:50:03 -0300

    Paravras reservadas da linguagem Java

---
+++ Aula_08.java
@@ -0,0 +1,11 @@
+package aulas;
+public class Aula_08{
+    
+    //Plavras reservadas são palavras chave da linguagem Java de origem inglesa.
+    public static void main(String[]args){
+        
+       int x = 1;
+       
+       System.out.println("Eu tenho " + x + " Pão com mortadela");
+    }
+}
