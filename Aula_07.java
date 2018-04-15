9bc2b7b966d0ec4a587c3d1cbc9e7f67dfb17aff
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-15 14:31:49 -0300

    Existem 8 tipos de dados primitivos em Java

---
+++ Aula_07.java
@@ -0,0 +1,22 @@
+package aulas;
+public class Aula_07{
+    
+    public static void main(String[]args){
+        
+        //Existem 8 tipos de dados primitivos em Java
+        //Tipos inteiros
+        byte a;
+        short b;
+        int c;
+        long d;
+        
+        float e;
+        double f;
+        
+        boolean g; // V ou F
+        char h = 'c'; //Armazena um único caractere
+        
+        
+        
+    }
+}
