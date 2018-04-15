b754de666be770d9b2e6a72a0909b4f03e524d31
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-15 17:51:43 -0300

    Casting

---
+++ Aula_12.java
@@ -0,0 +1,30 @@
+package aulas;
+public class Aula_12{
+    
+    public static void main(String[]args){
+        
+        int x = 10;
+        int y = 6;
+        int z = 3;
+        
+        double a = 3.0;
+        double b = 1.0;
+        double c = (double) x/y;
+        
+        //casting - força uma determinavel variável a ser de um determinado tipo
+        
+        System.out.println("x = " + x);
+        System.out.println("y = " + y);
+        System.out.println("z = " + z);
+        
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("x = " + c);
+        
+        
+        
+    }
+    
+    
+    
+}
