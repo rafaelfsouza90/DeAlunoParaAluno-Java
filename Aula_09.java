2899123fa7c0d21f023408da6ee0cdb70f73e9b7
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-15 15:32:11 -0300

    Calcular a área de um retângulo

---
+++ Aula_09.java
@@ -0,0 +1,23 @@
+package aulas;
+//Classe Aula_09
+public class Aula_09{
+    
+    //Função principal
+    public static void main(String[]args){
+        
+        //variáveis
+        double base;
+        double altura;
+        double areaRetangulo;
+        
+        //Atribuindo valores as variáveis
+        base = 10.34;
+        altura = 14.54;
+        //calculando a área do retângulo
+        areaRetangulo = base * altura;
+        
+        System.out.print("A area do retangulo de base = " + base);
+                System.out.print(" e altura = " + altura);
+                System.out.println(" é: " + areaRetangulo);
+    }
+}
