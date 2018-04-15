534e9f6ae70df8201c41d9428c534ebca0d969c7
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-15 15:49:03 -0300

    Importar pacotes/classes

---
+++ Aula_10.java
@@ -0,0 +1,18 @@
+package aulas;
+
+
+import java.util.Scanner; //Importa apenas a Classe Scanner dentro do pacote java.util
+// import java.util.*; //todas as Classes contidas dentro do pacote java.util são importadas
+//import java.lang // está contido automaticamente sem necessidade de importa-lo.
+
+public class Aula_10 {
+    
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        
+        
+        
+        
+    }
+    
+}
