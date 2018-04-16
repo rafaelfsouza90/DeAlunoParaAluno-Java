7289cf8cebf5bc01ad914c24f0e4f4c688f3654a
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-15 21:36:51 -0300

    Atalhos de Operadores

---
+++ Aula_16.java
@@ -0,0 +1,18 @@
+package aulas;
+public class Aula_16{
+    
+    public static void main(String[]args){
+        int x = 10;
+        
+        //x = x+5;
+        //Atalho para multiplicão
+        x*=5; 
+       // x/=5 //Atalho da divisão 
+       // x++; pós-incremento
+       // ++x; pré-incremento
+       // x--; pós-decremento
+       // --x; pré-decremento 
+        
+        System.out.println(x);
+    }
+}
