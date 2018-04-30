652ab813fa95a07368423ac640e18e3baa7226b4
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-04-30 13:58:13 -0300

    Ciclo/Laço FOR
    Ciclo/Laço FOR

---
+++ Aula_24.java
@@ -0,0 +1,12 @@
+package aulas;
+
+public class Aula_24 {
+    public static void main(String[]args){
+        
+        //Primeiro, valor inicial; Segundo, condição; Terceiro incremento
+        for(int i=0;i<100;++i){
+            System.out.println(i);
+        }
+    }
+    
+}
