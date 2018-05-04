eca69381ebc3b58c742e3f9ff339c1056ffce2fa
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-05-04 14:48:47 -0300

    Pré e Pós Incremento e Decremento.
    Pré e Pós Incremento e Decremento.

---
+++ Aula_29.java
@@ -0,0 +1,19 @@
+package aulas;
+
+public class Aula_29{
+    public static void main(String[]args){
+        
+        int a=10;
+        int b=a++;
+        
+        System.out.println(a);
+        System.out.println(b);
+        //++a; pré incremento
+        //a++; prós incremento
+        //--a; pré decremento
+        //a--; prós decremento
+          
+    }
+    
+    
+}
