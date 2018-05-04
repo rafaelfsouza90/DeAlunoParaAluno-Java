bdcba414e7de30825ebdb9b6f2947c63877dea33
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-05-04 19:12:00 -0300

    VetoresArrays pt. 1
    VetoresArrays pt. 1

---
+++ Aula_30.java
@@ -0,0 +1,22 @@
+package aulas;
+public class Aula_30{
+    
+    public static void main(String[]args){
+        //vetores ou arrays - Estamos falando da mesma coisa.
+        //criando um programa para armazenar notas de um determinado numero de alunos.
+        
+        int numAlunos = 5;
+        double[] notaAlunos = new double[numAlunos];
+        
+        notaAlunos[0] = 6.7;
+        notaAlunos[1] = 5.7;
+        notaAlunos[2] = 8.2;
+        notaAlunos[3] = 9.8;
+        notaAlunos[4] = 8.0;
+        
+        System.out.println(notaAlunos[0]);
+        
+        
+        
+    }
+}
