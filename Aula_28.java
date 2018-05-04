d6a3f29b81551550f53f4a00c5de7962863353c1
Rafael Souza <37389147+rafaelfsouza90@users.noreply.github.com>
2018-05-04 14:01:11 -0300

    Operador Condicional
    Operador Condicional

---
+++ Aula_28.java
@@ -0,0 +1,18 @@
+package aulas;
+
+public class Aula_28{
+    public static void main(String[]args){
+        
+        int a=10;
+        int b=20;
+        int maior;
+        
+        maior=(a<b) ? 500 : 1000; //retorna se acondição verdeira ou falsa.
+        
+        System.out.println(maior);
+                
+    }
+    
+    
+    
+}
